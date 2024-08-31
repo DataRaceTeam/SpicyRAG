3808cb3feaae56576de9a98ea552cf501e0a6691
ekaterina.baru <ekaterina.baru@velotix.ai>
2024-08-31 19:26:30 +0300

    added nvidia request

--- interface/main.py
+++ interface/main.py
@@ -3,6 +3,7 @@ from interface.database import engine
 from interface import utils, models, schemas
 import yaml
 import logging
+from openai import OpenAI
 
 # Load configuration
 config_path = "interface/config.yaml"
@@ -19,10 +20,39 @@ logger = logging.getLogger(config["project"]["name"])
 
 app = FastAPI(title=config["project"]["name"])
 
+# Fetch llm settings from config
+openai_base_url = config["llm"]["base_url"]
+openai_api_key = config["llm"]["api_key"]
+openai_model = config["llm"]["model"]
+openai_role = config["llm"]["role"]
+openai_temperature = config["llm"]["temperature"]
+openai_top_p = config["llm"]["top_p"]
+openai_max_tokens = config["llm"]["max_tokens"]
+
+# Initialize llm Client
+client = OpenAI(base_url=openai_base_url, api_key=openai_api_key)
+
 
 @app.post("/ask/")
 def ask_question(question: schemas.QuestionCreate):
     logger.info(f"Received question: {question.text}")
     word_count = utils.count_words(question.text)
     logger.info(f"Word count: {word_count}")
-    return {"word_count": word_count}
+
+    completion = client.chat.completions.create(
+        model=openai_model,
+        messages=[{"role": openai_role, "content": question.text}],
+        temperature=openai_temperature,
+        top_p=openai_top_p,
+        max_tokens=openai_max_tokens,
+        stream=True,
+    )
+
+    response_content = ""
+    for chunk in completion:
+        if chunk.choices[0].delta.content is not None:
+            response_content += chunk.choices[0].delta.content
+
+    logger.info(f"LLM Response: {response_content}")
+
+    return {"word_count": word_count, "llm_response": response_content}
