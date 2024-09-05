6d02f8f3b2125b94c4f4f5163811f8960d932d5a
maverchenko <mark.averchenko@gmail.com>
2024-09-05 22:40:27 +0500

    added semantic chunker

--- interface/chunker.py
+++ interface/chunker.py
@@ -1,18 +1,18 @@
 from abc import ABC, abstractmethod
 
 from langchain_core.documents import Document
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 class AbstractBaseChunker(ABC):
-
     @abstractmethod
     def chunk(self, text: str) -> list[str]:
         pass
 
 
 class RecursiveCharacterTextSplitterChunker(AbstractBaseChunker):
-
     def __init__(self, chunk_size, chunk_overlap, separators=None):
         super().__init__()
         self.text_splitter = RecursiveCharacterTextSplitter(
@@ -20,15 +20,34 @@ class RecursiveCharacterTextSplitterChunker(AbstractBaseChunker):
             chunk_overlap=chunk_overlap,
             add_start_index=True,
             is_separator_regex=False,
-            separators=separators
+            separators=separators,
         )
 
     def chunk(self, text: str) -> list[str]:
-        return [d.page_content for d in self.text_splitter.split_documents([Document(text)])]
+        return [
+            d.page_content for d in self.text_splitter.split_documents([Document(text)])
+        ]
+
+
+class SemanticTextChunker(AbstractBaseChunker):
+    def __init__(
+        self,
+        breakpoint_threshold_type="percentile",
+        breakpoint_threshold_amount=None,
+        number_of_chunks=None,
+    ):
+        self.splitter = SemanticChunker(
+            OpenAIEmbeddings(),
+            breakpoint_threshold_type=breakpoint_threshold_type,
+            breakpoint_threshold_amount=breakpoint_threshold_amount,
+            number_of_chunks=number_of_chunks,
+        )
+
+    def chunk(self, text: str) -> list[str]:
+        return self.splitter.split_text(text)
 
 
 class AbstractTransformChunk(ABC):
-
     @abstractmethod
     def transform(self, chunks: list[str], text: str) -> list[str]:
         pass
@@ -38,6 +57,6 @@ class AddHeaderTransformChunk(AbstractTransformChunk):
     """Add header from document to chunk"""
 
     def transform(self, chunks: list[str], text: str) -> list[str]:
-        header = text[:[i for i in range(len(text)) if text[i] == '"'][1] + 1]
+        header = text[: [i for i in range(len(text)) if text[i] == '"'][1] + 1]
 
         return [header + c for c in chunks]
