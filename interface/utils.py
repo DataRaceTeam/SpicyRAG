ca45082cd08351db913610b03f6a36a23a423019
Daniyar <46517339+smvkvv@users.noreply.github.com>
2024-09-11 22:26:35 +0300

    remove-log

--- interface/utils.py
+++ interface/utils.py
@@ -155,9 +155,7 @@ def load_and_process_text_documents(db, embedder: Embedder, es: Elasticsearch, c
 
         create_index(index_name="chunks", es_client=es)
         logger.info("Started vectorizing the NPA data and store it in database")
-        for i, document in enumerate(content):
-            if i % 20 == 0:
-                logger.info(f"Finished [{i}/{len(content)}] document")
+        for document in content:
             text = document.strip()
 
             date = extract_date(text)
